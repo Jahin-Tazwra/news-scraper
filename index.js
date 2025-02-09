67c71f7c08d5dca5a664942a88c86e266fced985
Jahin-Tazwra <tazwarjahin@gmail.com>
2025-02-09 23:39:52 +0600

    Update index

--- index.js
+++ index.js
@@ -1,64 +1,54 @@
-const express = require("express");
+const app = require("express")();
 const puppeteer = require("puppeteer");
-
-const app = express();
 
 // Custom delay function
 function delay(time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
+  return new Promise(resolve => setTimeout(resolve, time));
 }
 
 app.get("/api/:ticker", async (req, res) => {
-  const ticker = req.params.ticker;
-  const options = {
-    headless: true,
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--disable-gpu",
-      "--no-zygote",
-    ],
+  let options = {
     executablePath:
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
   };
+  const ticker = req.params.ticker;
 
   try {
     const browser = await puppeteer.launch(options);
     const page = await browser.newPage();
 
-    await page.goto(`https://finance.yahoo.com/quote/${ticker}/news`, {
-      waitUntil: "networkidle2",
-      timeout: 500000
-    });
+    await page.goto(`https://finance.yahoo.com/quote/${ticker}/news`, {timeout: 100000});
 
-    const selector =
-      "#nimbus-app > section > section > section > article > section.mainContent.yf-tnbau3 > section";
-    await page.waitForSelector(selector, { timeout: 500000 });
+    // Wait for the target element to load
+    const selector = "#nimbus-app > section > section > section > article > section.mainContent.yf-tnbau3 > section";
+    await page.waitForSelector(selector, {timeout: 100000});
 
+    // Scroll 5 times to load more content, waiting 2 seconds between each scroll
     for (let i = 0; i < 2; i++) {
       await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
       await delay(2000);
     }
 
+    // After scrolling, select the element again
     const element = await page.$(selector);
-    const elementHtml = await page.evaluate(
-      (el) => el?.outerHTML || "",
-      element
-    );
 
+    // Extract the element's outer HTML
+    const elementHtml = await page.evaluate(el => el.outerHTML, element);
+
+    // Return the HTML of the selected element
     res.send(elementHtml);
+
     await browser.close();
   } catch (err) {
-    console.error("Error occurred:", err);
+    console.error(err);
     res.status(500).send("An error occurred while scraping the page.");
   }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
 });
 
 module.exports = app;
